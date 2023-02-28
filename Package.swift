2a7206769d3fea733afa664d62ca18af23dc8966
vladislav <>
2023-02-28 18:47:31 +0400

    update package.swift, make virtual wrapper

--- Package.swift
+++ Package.swift
@@ -5,11 +5,24 @@ let package = Package(
     name: "Bytix",
     platforms: [.iOS(.v12)],
     products: [
-        .library(name: "Bytix",
-                 targets: ["Bytix"])
+        .library(
+            name: "Bytix",
+            targets: ["Bytix", "BytixWrapper"]
+        )
+    ],
+    dependencies: [
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0")
     ],
     targets: [
-        .binaryTarget(name: "Bytix",
-                      path: "Bytix.xcframework")
+        .target(
+            name: "BytixWrapper",
+            dependencies: [
+                "SwiftProtobuf"
+            ]
+        ),
+        .binaryTarget(
+            name: "Bytix",
+            path: "Bytix.xcframework"
+        )
     ]
 )
