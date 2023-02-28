2afde1647cbacd35a77e719f04ed60ea12a57cd9
vladislav <>
2023-02-28 17:37:18 +0400

    add package.swift file for support SwiftPM

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Bytix",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(name: "Bytix",
+                 targets: ["Bytix"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0"),
+    ],
+    targets: [
+        .binaryTarget(name: "Bytix",
+                      path: "Bytix.xcframework",
+                      dependencies: [.product(name: "SwiftProtobuf",
+                                              package: "swift-protobuf")])
+    ]
+)
